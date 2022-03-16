52f185c59736f769de2aef3a38b952016bf725a7
Jancer Lima <contato.jancersl@gmail.com>
2022-03-16 08:04:06 -0300

    Aula[3]

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,8 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
 import Home from "./pages/Home";
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Home />
-    </View>
-  );
+  return <Home />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
 
 export default App;
--- src/app/pages/Home/index.tsx
+++ src/app/pages/Home/index.tsx
@@ -1,29 +1,102 @@
-import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+import Realm from "realm";
 
 import getRealm from "../../../infrastructure/realm";
 import { ITask } from "../../../business/models/interfaces/ITask";
 import getAllTasks from "../../../business/services/getAllTasks";
 
 const Home = () => {
-  const getAll = async () => {
-    const data = await getAllTasks();
-    console.log(data);
+  const [taskName, setTaskName] = useState("");
+
+  const [data, setData] = useState<
+    Realm.Results<ITask & Realm.Object> | ITask[]
+  >([]);
+
+  useEffect(() => {
+    const cleanup = (async () => {
+      const realm = await getRealm();
+
+      const tasks = realm.objects<ITask>("Task");
+
+      setData(tasks);
+
+      tasks.addListener((values) => {
+        setData([...values]);
+      });
+
+      return () => {
+        tasks.removeAllListeners();
+        realm.close();
+      };
+    })();
+
+    return () => {
+      cleanup.then((func) => func());
+    };
+  }, []);
+
+  const addTask = async () => {
+    const realm = await getRealm();
+    realm.write(() => {
+      realm.create<ITask>("Task", {
+        _id: data.length + 1,
+        name: taskName,
+        status: "Created now",
+      });
+    });
+    // setTaskName("");
   };
 
-  const deleteTask = async () => {
-    const realm = await getRealm();
+  const getAll = async () => {
+    console.log(await getAllTasks());
+  };
 
+  const deleteAll = async () => {
+    const realm = await getRealm();
     realm.write(() => {
-      realm.delete(realm.objects<ITask>("Task").filtered("_id = 1"));
+      realm.deleteAll();
     });
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Realm Delete</Text>
-      <Button title="Get All Tasks" onPress={getAll} />
-      <Button title="Delete Task" onPress={deleteTask} />
+      <Text style={styles.text}>Realm Listen</Text>
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          value={taskName}
+          onChangeText={(value) => setTaskName(value)}
+        />
+        <Button title="Add" onPress={addTask} />
+      </View>
+
+      <Text style={styles.textInfo}>Stored data</Text>
+
+      <FlatList
+        data={data}
+        style={styles.flatList}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.itemContainer}>
+              <Text style={styles.itemText}>{item.name}</Text>
+            </View>
+          );
+        }}
+      />
+      <View style={styles.buttonContainer}>
+        <Button title="Get all" onPress={getAll} />
+        <Button title="Delete all" onPress={deleteAll} />
+      </View>
     </View>
   );
 };
@@ -38,6 +111,48 @@ const styles = StyleSheet.create({
     marginBottom: 30,
     fontSize: 30,
     fontWeight: "bold",
+  },
+  textInfo: {
+    fontSize: 25,
+    marginBottom: 10,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-around",
+    marginHorizontal: 10,
+  },
+  input: {
+    width: "80%",
+    borderWidth: 2,
+    borderColor: "grey",
+    fontSize: 20,
+    padding: 5,
+  },
+  flatList: {
+    flexGrow: 0,
+    width: "90%",
+    height: "50%",
+    borderWidth: 2,
+    borderColor: "grey",
+  },
+  itemContainer: {
+    flexDirection: "row",
+    marginHorizontal: 5,
+    paddingHorizontal: 10,
+    borderBottomWidth: 1,
+    borderColor: "grey",
+  },
+  itemText: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "50%",
+    justifyContent: "space-between",
+    margin: 10,
   },
 });
 
